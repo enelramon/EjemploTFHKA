573d8b0b34eae55347c405a87b4d17c87c719b6b
Enel Almonte <enelramon@hotmail.com>
2021-01-31 19:34:20 -0400

    Perparando ejemplos para Lilyanna

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,65 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.FacturaFirmaButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PuertoTextBox = New System.Windows.Forms.TextBox()
+        Me.NCButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'FacturaFirmaButton
+        '
+        Me.FacturaFirmaButton.Location = New System.Drawing.Point(49, 63)
+        Me.FacturaFirmaButton.Name = "FacturaFirmaButton"
+        Me.FacturaFirmaButton.Size = New System.Drawing.Size(139, 36)
+        Me.FacturaFirmaButton.TabIndex = 25
+        Me.FacturaFirmaButton.Text = "Factura Consumidor Final"
+        Me.FacturaFirmaButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 27
+        Me.Label1.Text = "Puerto"
+        '
+        'PuertoTextBox
+        '
+        Me.PuertoTextBox.Location = New System.Drawing.Point(49, 12)
+        Me.PuertoTextBox.Name = "PuertoTextBox"
+        Me.PuertoTextBox.Size = New System.Drawing.Size(78, 20)
+        Me.PuertoTextBox.TabIndex = 26
+        Me.PuertoTextBox.Text = "4"
+        '
+        'NCButton
+        '
+        Me.NCButton.Location = New System.Drawing.Point(49, 121)
+        Me.NCButton.Name = "NCButton"
+        Me.NCButton.Size = New System.Drawing.Size(139, 36)
+        Me.NCButton.TabIndex = 28
+        Me.NCButton.Text = "Nota de Credito"
+        Me.NCButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(454, 296)
+        Me.Controls.Add(Me.NCButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PuertoTextBox)
+        Me.Controls.Add(Me.FacturaFirmaButton)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Private WithEvents FacturaFirmaButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PuertoTextBox As TextBox
+    Private WithEvents NCButton As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,110 @@
-﻿Public Class Form1
+﻿Imports TfhkaNet.IF.RD
 
+Public Class Form1
+
+    Private Impresora As New Tfhka
+
+    Private Sub FacturaFirmaButton_Click(sender As Object, e As EventArgs) Handles FacturaFirmaButton.Click
+        If AbrirPuerto() = False Then
+            Return
+        End If
+
+        EnviarComando("F00000000B1500002000")
+
+        EnviarComando("iR0404000316") 'RNC
+
+        EnviarComando("iS0AYUNTAMIENTO MUNICIPAL COTUI") 'NOMBRE
+        EnviarComando("i00CODIGO: 641") 'CODIGO
+        EnviarComando("i01DIR: SANCHEZ") 'DIRECCION
+
+        EnviarComando("/0") 'Consumidor final
+
+        'envio estas descriciones aqui, porque si pasa algo luego no las puedo imprimir.
+        EnviarComando("i00NUMERO: 707825")
+        EnviarComando("y0000707825")
+        '----------------------------------------------
+
+        EnviarComando("""000008000000003000Ref: 12458") 'PRECIO=800.00  X CANTIDAD= 30.00
+        EnviarComando("@PINO 2X4X16 BT")                'Descripcion adicional para el articulo
+        EnviarComando("""000017600000015000Ref: 13729") 'PRECIO=1760.00  X CANTIDAD= 150.00
+        EnviarComando("@PLYWOOD HIDROFUGO 3/4")
+
+        EnviarComando("4") 'totalizar sin imprimir
+
+        EnviarComando("207000030000000") 'Pagar 300,000.00
+        'ERROR IMPORTANTE: si envio 101 las descripciones adicionales no salen.
+
+        Dim S1 As S1PrinterData = Impresora.getS1PrinterData()
+
+        Dim Numero = S1.getQuantityNonFiscalDocuments().ToString()
+        Dim Serial = S1.getRegisteredMachineNumber()
+
+        'USARE ESTOS VALORES PARA SABER EL NIF GENERADO.
+        EnviarComando("@Serial:" & Serial)
+        EnviarComando("@Secuencia:" & Numero)
+
+        EnviarComando("199") 'cerrar el documento
+
+    End Sub
+
+    Private Sub NCButton_Click(sender As Object, e As EventArgs) Handles NCButton.Click
+
+        If AbrirPuerto() = False Then
+            Return
+        End If
+
+        EnviarComando("iS0ENEL RAMON ALMONTE PICHARDO PICHARDO PICHARDO")
+        EnviarComando("iR005601453607")
+        EnviarComando("F00000000B0400022597")
+        EnviarComando("iF000000000B0200022597")
+
+        EnviarComando("/2")
+
+        EnviarComando("""000017600000015000Referencia: 13729")
+        EnviarComando("@PLYWOOD HIDROFUGO 3/4")
+
+        EnviarComando("4") 'totalizar sin imprimir
+
+        EnviarComando("@ESTOS COMENTARIOS NO IMPRIMEN")
+        EnviarComando("@ESTOS COMENTARIOS NO IMPRIMEN")
+        EnviarComando("@ESTOS COMENTARIOS NO IMPRIMEN")
+
+        EnviarComando("199")
+    End Sub
+
+    Function AbrirPuerto() As Boolean
+        Dim paso As Boolean = False
+        If (Impresora.StatusPort) Then
+            Impresora.CloseFpctrl()
+            MsgBox("El puerto ya estaba abierto")
+        End If
+
+        If Impresora.OpenFpctrl("COM" & PuertoTextBox.Text) Then
+
+            Impresora.getS2PrinterData()
+            If (Impresora.S2Estado1.getTypeDocument <> 0) Then
+                EnviarComando("7", "cancelar") 'cancelar
+            End If
+
+            paso = True
+        End If
+
+        Return paso
+    End Function
+
+
+    Private Function EnviarComando(Comando As String, Optional obsevacion As String = "") As Boolean
+        Dim Retorno As Boolean
+
+        Retorno = Impresora.SendCmd(Comando)
+
+        If Retorno = False Then
+            Impresora.ReadFpStatus()
+            MsgBox("El comando: " & Comando & vbCrLf &
+                   "Dio el error: " & Impresora.Status_Error)
+        End If
+
+        Return Retorno
+
+    End Function
 End Class
